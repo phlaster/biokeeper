e76678539a83b772948f5ea8e2b86b1665da6b2a
Alex <curjucklloyd@gmail.com>
2023-05-28 19:29:35 +0300

    differential response for /req/

--- src/stuff/serv_SIMPLE.py
+++ src/stuff/serv_SIMPLE.py
@@ -1,19 +1,47 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from datetime import datetime
 import time
 
 hostName = "0.0.0.0" #"localhost"
 serverPort = 8080
 
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
+def isrequest(path):
+    return path.startswith('/req/')
 
+def log_successfull(msg):
+    with open("logs.log", "a") as log:
+        print(datetime.now(), msg.path[5:], file=log)
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+
+    def do_GET(self):
+        if isrequest(self.path):
+            log_successfull(self)
+            
+            self.send_response(200)
+        else:
+            self.send_response(400)
+
+        self.end_headers()
         print(self.path)
-        
-        # self.send_header("Content-type", "text/html")
-        # self.end_headers()
+
+    # def do_POST(self):
+    #     content_length = int(self.headers['Content-Length'])
+    #     body = self.rfile.read(content_length)
+    #     self.send_response(200)
+    #     self.send_header('Content-type', 'text/html')
+    #     self.end_headers()
+    #     response = BytesIO()
+    #     response.write(b'This is POST request. ')
+    #     response.write(b'Received: ')
+    #     response.write(body)
+    #     self.wfile.write(response.getvalue())
+
+
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
@@ -23,7 +51,6 @@ if __name__ == "__main__":
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
-
-    webServer.server_close()
-    print("Server stopped.")
-#
+    finally:
+        webServer.server_close()
+        print("Server stopped.")
