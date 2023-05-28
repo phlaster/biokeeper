26e6ad0344ab851cc6f37e5fc0f212eb9f871cf2
Alex <curjucklloyd@gmail.com>
2023-05-28 16:49:26 +0300

    extracted requirements

--- src/server.py
+++ src/server.py
@@ -3,12 +3,13 @@ import psycopg2
 
 
 
-conn = psycopg2.connect(database=db["db_name"],
-                        host=db["db_host"],
-                        user=db["db_user"],
-                        password=db["db_pass"],
-                        port=db["db_port"]
-                        )
+conn = psycopg2.connect(
+    database=db["db_name"],
+    host=db["db_host"],
+    user=db["db_user"],
+    password=db["db_pass"],
+    port=db["db_port"]
+)
 
 cursor = conn.cursor()
 
