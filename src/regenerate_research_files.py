aa892a2d0b0dfd2cb47c6ef91cccbdc887f2b8b0
Alex <curjucklloyd@gmail.com>
2023-05-30 00:12:06 +0300

    now asserts, that research with such id exists

--- src/regenerate_research_files.py
+++ src/regenerate_research_files.py
@@ -14,29 +14,35 @@ def date_to_str(d):
 
 
 def extract_research_params(connection, id):
-    base = connection.cursor()
-    base.execute(f"SELECT * FROM reseach WHERE id_res={id}")
-    got = base.fetchone()
+    try:
+        base = connection.cursor()
+        base.execute(f"SELECT * FROM reseach WHERE id_res={id}")
+        got = base.fetchone()
+        assert got != None, f"No research with id {id} was found!" 
 
-    base.execute("SELECT COUNT(*) FROM sampl WHERE id_res < %s", (id,))
-    offset = int(base.fetchone()[0])
+        base.execute("SELECT COUNT(*) FROM sampl WHERE id_res < %s", (id,))
+        offset = int(base.fetchone()[0])
 
-    base.close()
-    extracted = (
-        int(got[0]),
-        date_to_str(got[3]),
-        date_to_str(got[4]),
-        got[1],
-        int(got[2]),
-        offset
-    )
-    return extracted
+        
+        extracted = (
+            int(got[0]),
+            date_to_str(got[3]),
+            date_to_str(got[4]),
+            got[1],
+            int(got[2]),
+            offset
+        )
+        return extracted
+    finally:
+        print("after")
+        base.close()
+    
 
 
 def main():
     try:
         connection, dbase = connect2db(db)
-        exparams = extract_research_params(connection, argv[1])
+        params = extract_research_params(connection, argv[1])
         r = Research(*params)
         r.write_codes()
         r.write_pictures()
