40fd0862a782a4d887431ddf802108923f4b91c9
Alex <curjucklloyd@gmail.com>
2023-11-11 11:40:27 +0300

    clean code

--- src/Research.py
+++ src/Research.py
@@ -6,59 +6,58 @@ import os
 from sys import stderr
 
 class Research:
-    def __init__(self, id, date_start, date_end, research_type, n_samples, offset):
-        self.id = id
+    def __init__(self, research_id, date_start, date_end, research_type, n_samples, offset):
+        self.research_id = research_id
         self.date_start = date_start
         self.date_end = date_end
         self.research_type = research_type
         self.n_samples = n_samples
         self.offset = offset
-        self._qrs = _generate_qrs_pure()
-
-        # self._generate_qrs()
+        self._qrs = self._generate_qrs()
 
 
-    def get_qrs(self):
+    def get_qrs(self) -> dict:
         return self._qrs
 
 
-    def _public_fields_digest(self):
+    def _public_fields_digest(self) -> str:
         fields = [str(v) for k, v in self.__dict__.items() if not k.startswith("_")]
         return "".join(fields)
 
 
-    # def _generate_qrs(self, length = 16):
-    #     random.seed(self._public_fields_digest())
-    #     for i in range(self.n_samples):
-    #         self._qrs[i+1+self.offset] = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(length))
-
-    def _generate_qrs_pure(self, length=16):
-        new_qrs = {}
+    def _generate_qrs(self, length=16) -> dict:
         random.seed(self._public_fields_digest())
-        for i in range(self.n_samples):
-            new_qrs[i + 1 + self.offset] = ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
+        a_z = string.ascii_lowercase
+        new_qrs = {i + 1 + self.offset : ''.join(random.choice(a_z) for _ in range(length)) for i in range(self.n_samples)}
         return new_qrs
 
-
     
-    def write_codes(self):
+    def write_codes(self) -> None:
         if not self._qrs:
-            print("No qr codes were generated for this research!", file=stderr)
+            print("No QRs in this research!", file=stderr)
         else:
-            if not os.path.isdir(f"./research{self.id}/"):
-                os.makedirs(f"./research{self.id}/")
-            with open(f"research{self.id}/research{self.id}_{self.n_samples}qrs.csv", "w") as f:
+            research_dir = f"research_{self.research_id}"
+
+            if not os.path.isdir(research_dir):
+                os.makedirs(research_dir)
+
+            with open(research_dir + os.sep + f"research={self.research_id}" + f"_qr={self.n_samples}.csv", "w") as f:
                 for i in range(self.n_samples):
                     print(f"{i+1+self.offset},{self._qrs[i+1+self.offset]}", file=f)
-            print(f"{self.n_samples} codes have been written!", file=stderr)
+
+            print(f"Codes {self.offset+1} to {self.offset+self.n_samples} have been written!", file=stderr)
 
 
-    def write_pictures(self):
+    def write_pictures(self) -> None:
         if not self._qrs:
             print("No qr codes were generated for this research!", file=stderr)
+
         else:
-            if not os.path.isdir(f"./research{self.id}/"):
-                os.makedirs(f"./research{self.id}/")
+            research_dir = f"research_{self.research_id}"
+            
+            if not os.path.isdir(research_dir):
+                os.makedirs(research_dir)
+
             for i in range(self.n_samples):
                 n = i+1+self.offset
                 qr = qrcode.QRCode(
@@ -69,12 +68,21 @@ class Research:
                 )
                 qr.add_data(self._qrs[n])
                 qr.make(fit=True)
-                qr.make_image(fill_color="black", back_color="white").save(f"research{self.id}/{n}.svg")
+                qr.make_image(fill_color="black", back_color="white").save(research_dir+os.sep+str(n) + ".svg")
             print(f"{self.n_samples} QR images have been written!", file=stderr)
+
 
 
 if __name__ == "__main__":
     print("Generating sample research files...")
-    r = Research(0,1,2,3,4,5)
-    r.write_codes()
-    r.write_pictures()
+    new_research = Research(
+        research_id=0,
+        date_start=1,
+        date_end=2,
+        research_type=3,
+        n_samples=4,
+        offset=5
+    )
+
+    new_research.write_codes()
+    new_research.write_pictures()
