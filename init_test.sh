259012515d337ba9706daa6c655666c86d1ec22e
Alex <curjucklloyd@gmail.com>
2023-11-22 13:51:48 +0300

    script for testing (like init, but no server run)

---
+++ init_test.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+apt update;
+apt upgrade -y;
+apt install -y git python3 python3-pip;
+git clone https://$GIT_TOKEN@github.com/phlaster/opd.git;
+cd opd;
+git checkout backend;
+pip install -r requirements.txt
