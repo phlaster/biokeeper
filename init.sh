cfc89167d5ba4ef7fcb2ee3a489f3fc01c8fbb6e
Alex <curjucklloyd@gmail.com>
2023-11-22 14:04:47 +0300

    now clonning only single backend branch from repo

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 apt update;
 apt upgrade -y;
 apt install -y git python3 python3-pip;
-git clone https://$GIT_TOKEN@github.com/phlaster/opd.git;
+git clone -b backend --single-branch https://$GIT_TOKEN@github.com/phlaster/opd.git;
 cd opd;
 git checkout backend;
 pip install -r requirements.txt
