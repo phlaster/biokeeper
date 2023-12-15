e3019104845b39b8783d90f6348dafea23c8d54c
Alex <curjucklloyd@gmail.com>
2023-12-15 22:35:01 +0300

    no need in init scripts now using Dockerfile and docker-compose

--- init_test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-apt update;
-apt upgrade -y;
-apt install -y git python3 python3-pip;
-git clone -b backend --single-branch https://$GIT_TOKEN@github.com/phlaster/opd.git;
-cd opd;
-git checkout backend;
-pip install -r requirements.txt
