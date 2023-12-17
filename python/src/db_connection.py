16244777b9312c992111567b485217d53082328b
Alex <curjucklloyd@gmail.com>
2023-12-17 13:35:59 +0300

    removed unused function

--- python/src/db_connection.py
+++ python/src/db_connection.py
@@ -1,12 +1,5 @@
 import psycopg2
 
-def running_in_docker():
-    try:
-        with open('/proc/1/cgroup', 'r') as f:
-            return any('docker' in line for line in f)
-    except FileNotFoundError:
-        return False
-
 def connect2db(logdata):
     connection = psycopg2.connect(
         database = logdata["db_name"],
@@ -14,5 +7,5 @@ def connect2db(logdata):
         user =     logdata["db_user"],
         password = logdata["db_pass"],
         port =     logdata["db_port"])
-    dbhandle = connection.cursor()
-    return connection, dbhandle
+    cursor = connection.cursor()
+    return connection, cursor
