6ecdac2c1046e5899839290f66467daee03d9d34
Rimk4 <82030462+Rimk4@users.noreply.github.com>
2024-05-06 18:36:26 +0300

    Add files via upload

---
+++ components/Take_photo.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function Take_photo({ navigation }) {
+  const [cameraPermission, setCameraPermission] = useState(null);
+  const [galleryPermission, setGalleryPermission] = useState(null);
+
+  const [imageUri, setImageUri] = useState(null);
+
+  const permisionFunction = async () => {
+    // here is how you can get the camera permission
+    const cameraPermission = await Camera.requestCameraPermissionsAsync();
+
+    setCameraPermission(cameraPermission.status === 'granted');
+
+    const imagePermission = await ImagePicker.getMediaLibraryPermissionsAsync();
+    console.log(imagePermission.status);
+
+    setGalleryPermission(imagePermission.status === 'granted');
+
+    if (
+      imagePermission.status !== 'granted' &&
+      cameraPermission.status !== 'granted'
+    ) {
+      alert('Permission for media access needed.');
+    }
+  };
+
+  useEffect(() => {
+    permisionFunction();
+  }, []);
+
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+      presentationStyle: 0
+    });
+
+    console.log(result);
+    if (!result.canceled) {
+      setImageUri(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraContainer}>
+        <Text>Сделайте хорошее фото, и загрузите его из галереи.</Text>
+      </View>
+
+      <Button title={'Gallery'} onPress={pickImage} />
+      {imageUri && <Image source={{ uri: imageUri }} style={{ flex: 1 }} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  button: {
+    flex: 0.1,
+    padding: 10,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+});
