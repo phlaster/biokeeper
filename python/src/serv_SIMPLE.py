88f524d0b2b0c8c448f13d42ecf390d871fb8ddf
Name Name <you@example.com>
2024-05-07 20:49:13 +0300

    hotfix

--- python/src/serv_SIMPLE.py
+++ python/src/serv_SIMPLE.py
@@ -15,66 +15,6 @@ from colorama import Fore, Style
 
 DB_LOGDATA = DB_from_docker if is_docker() else DB
 
-def in_db(db_logdata, qr) -> bool:
-    try:
-        connection, cursor = connect2db(db_logdata)
-        cursor.execute(f"SELECT qr_text FROM generated_qrs WHERE qr_text='{qr}'")
-        x = cursor.fetchone()
-        return x != None
-    finally:
-        cursor.close()
-        connection.close()
-
-
-def is_used(db_logdata, qr) -> bool:
-    try:
-        connection, cursor = connect2db(db_logdata)
-        cursor.execute(f"SELECT qr_id FROM generated_qrs WHERE qr_text='{qr}'") # FINDS QR ID for given QR string
-        qr_id = int(cursor.fetchone()[0])
-
-        cursor.execute(f"SELECT qr_id FROM collected_samples WHERE qr_id={qr_id}")
-        x = cursor.fetchone()
-        return x != None
-    finally:
-        cursor.close()
-        connection.close()
-
-
-def is_expired(db_logdata, qr) -> bool:
-    try:
-        connection, cursor = connect2db(db_logdata)
-        cursor.execute(f"SELECT research_id FROM generated_qrs WHERE qr_text='{qr}'") # FINDS sample ID from QRcode
-        research_id = int(cursor.fetchone()[0])
-        cursor.execute(f"SELECT day_start, day_end FROM researches WHERE research_id={research_id}")
-        date_end = cursor.fetchone()[1]
-        return date_end < date.today()
-    finally:
-        cursor.close()
-        connection.close()
-
-
-def push_request(db_logdata, request) -> None:
-    qr = request[0:16]
-    req_body = request[17:].split('&')
-    temperature = round(float(req_body[0]))
-    location = req_body[1].strip('/')
-
-    try:
-        connection, cursor = connect2db(db_logdata)
-        cursor.execute(f"SELECT qr_id FROM generated_qrs WHERE qr_text='{qr}'")
-        qr_id = int(cursor.fetchone()[0])
-
-        cursor.execute("""
-            INSERT INTO collected_samples (qr_id, date, time, temperature, gps)
-            VALUES (%s, %s, %s, %s, POINT(%s));
-            """,
-            (qr_id, date.today(), datetime.now(), temperature, location)
-        )
-        connection.commit()
-    finally:
-        cursor.close()
-        connection.close()
-
 
 class MyServer(BaseHTTPRequestHandler):
     def _decide_request(self):
