813379d62aebf7001442e50e9fb019012dece446
Name Name <you@example.com>
2024-05-09 12:09:21 +0300

    upd

--- python/src/regenerate_research_files.py
+++ python/src/regenerate_research_files.py
@@ -23,9 +23,11 @@ def write_codes(qrs, research_id):
         os.makedirs(research_dir)
 
     N = len(qrs)
-    with open(research_dir + os.sep + f"research={research_id}" + f"_qr={N}.csv", "w") as f:
+    name = research_dir + os.sep + f"research={research_id}" + f"_qr={N}.csv"
+    with open(name, "w") as f:
         for (qr, id) in qrs:
             print(f"{id},{qr}", file=f)
+    return
 
 def write_pictures(qrs, research_id):
     research_dir = f"research_{research_id}"
@@ -33,15 +35,18 @@ def write_pictures(qrs, research_id):
         os.makedirs(research_dir)
 
     for (qr, id) in qrs:
-        qr_code = qrcode.QRCode(
+        qr_generator = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=25,
             border=2,
         )
-        qr_code.add_data(qr)
-        qr_code.make(fit=True)
-        qr_code.make_image(fill_color="black", back_color="white").save(research_dir+os.sep+str(id) + ".svg")
+        qr_generator.add_data(qr)
+        qr_generator.make(fit=True)
+
+        name = research_dir+os.sep+str(id) + ".svg"
+        qr_generator.make_image(fill_color="black", back_color="white").save(name)
+    return
 
 def main():
     research_id = sys.argv[1]
@@ -49,7 +54,7 @@ def main():
         qrs = retrieve_qrs(cursor, research_id)
         if len(qrs) == 0:
             print(f"No entries for research #{research_id} in database!", file=sys.stderr)
-            exit()
+            return
         write_codes(qrs, research_id)
         write_pictures(qrs, research_id)
         print(f"Research #{research_id} has been regenerated!", file=sys.stderr)
