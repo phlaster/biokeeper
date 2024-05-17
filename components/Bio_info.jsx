4b497bb032a2a4087d6f06933d3a3c0a62c44c08
Rimk4 <82030462+Rimk4@users.noreply.github.com>
2024-05-17 23:59:23 +0300

    Локальное хранилище(ещё в доработке)

--- components/Bio_info.jsx
+++ components/Bio_info.jsx
@@ -34,6 +34,20 @@ export default function Bio_info({ route, navigation }) {
       "Теперь можно сфотографировать местность"
     );
 
+    //---------------------------------------
+    let file = new Blob([JSON.stringify(location)], {type: "txt"});
+    let a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = "last_location";
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0);
+    //------------------------------------------
+    
     var request = new XMLHttpRequest();
     request.onreadystatechange = (e) => {
       if (request.readyState !== 4) {
