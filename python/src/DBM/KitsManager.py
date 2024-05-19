a5654d323709a2e4be5d98d0ca808cf39e309063
Name Name <you@example.com>
2024-05-19 18:14:34 +0300

    feat(KitsManager): Refactor `_generate_qr_bytes()`, `count()`, `has_status()`, `has()`, `status_of()`, `get_qrs()`, `get_info()`, `new()`, `change_status()`, and `change_owner()` methods

--- python/src/DBM/KitsManager.py
+++ python/src/DBM/KitsManager.py
@@ -1,158 +1,173 @@
 from DBM.ADBM import AbstractDBManager
 from DBM.UsersManager import UsersManager
 import os
+from multimethod import multimethod
 
 class KitsManager(AbstractDBManager):
-    def _generate_qr_bytes(self, n: int, l: int):
-        return [os.urandom(l) for _ in range(n)]
+    def _generate_qr_bytes(self, n: int, l: int = 10):
+        """
+        Generate a list of n random hexadecimal strings of length l.
+        Returns a list of strings.
+        """
+        return [os.urandom(l).hex() for _ in range(n)]
 
-
-
+    @multimethod
     def count(self, status: str = "all"):
         """
-        Returns number of kits with different statuses.
-        'all' for all statuses OR
-        Currently possible statuses: "created", "sent", "activated"
+        Returns the number of kits with the specified status.
+        If status is 'all', returns the total number of kits.
+        Returns an integer.
         """
         return self._counter("kit_statuses", status)
 
-    
-    def has_status(self, status):
+    @multimethod
+    def has_status(self, status: str):
+        """
+        Checks if the given status is valid for kit.
+        Returns a (id, description) tuple if valid, otherwise ().
+        """
         return self._is_status_of("kit", status)
 
-    
-    def has(self, kit_id):
-        return self._is("kit_id", "kits", "kit_id", kit_id)
+    @multimethod
+    def has(self, kit_id: int, log=False):
+        """
+        Checks if a kit with the given ID exists.
+        Returns the kit ID if it exists, otherwise 0.
+        """
+        id = self._SELECT("id", "kit", "id", kit_id)
+        if not id:
+            return self.logger.log(f"Error: No kit #{kit_id}.", 0) if log else 0
+        return id
 
-    
-    def status_of(self, kit_id):
+    @multimethod
+    def has(self, unique_hex: str, log=False):
         """
-        -- logging --
-        Returns the status of a kit with the given kit_id.
-        If the kit does not exist, returns False.
+        Checks if a kit with the given unique hexadecimal code exists.
+        Returns the kit ID if it exists, otherwise 0 or logs an error message.
         """
-        if not self.has(kit_id):
-            self.logger.log_message(f"Error: Kit #{kit_id} does not exist.")
-            return ""
-        return self._status_getter("kit_status", "kits", "kit_id", "kit_statuses", kit_id)
+        id = self._SELECT("id", "kit", "unique_hex", unique_hex)
+        if not id:
+            return self.logger.log(f"Error: No kit with hex '{unique_hex}'.", 0) if log else 0
+        return id
 
-    
-    def get_qrs(self, kit_id: int):
+    def status_of(self, identifier, log=False):
         """
-        -- logging --
-        Returns a list of tuples containing (qr_id, qr_unique_code) for the kit with the given kit_id.
-        If the kit does not exist, returns empty dict.
+        Returns the status of the kit with the given identifier (ID or unique hexadecimal code).
+        If the kit does not exist, returns an empty string.
+        """
+        kit_id = self.has(identifier)
+        if not id:
+            return self.logger.log(f"Error: Kit #{kit_id} does not exist.", "") if log else ""
+        return self._status_getter("kit", kit_id)
+
+    def get_qrs(self, identifier, log=False):
+        """
+        Returns a dictionary mapping QR IDs to their unique hexadecimal codes for the kit with the given identifier.
+        If the kit does not exist, returns an empty dictionary.
         """
         qr_info = {}
-        if not self.has(kit_id):
-            self.logger.log_message(f"Error: Kit #{kit_id} does not exist.")
-            return qr_info
-
+        kit_id = self.has(identifier)
+        if not kit_id:
+            return self.logger.log(f"Error: Kit #{kit_id} does not exist.", qr_info) if log else qr_info
 
         with self.db as (conn, cursor):
-            cursor.execute("SELECT qr_id, qr_unique_code FROM qrs WHERE kit_id = %s", (kit_id,))
+            cursor.execute(
+                """SELECT id, unique_hex FROM "qr" WHERE kit_id = %s""", (kit_id,))
             qr_info_tuples = cursor.fetchall()
 
-        for qr_id, qr_unique_code in qr_info_tuples:
-            qr_info[qr_id] = bytes(qr_unique_code).hex()
+        for qr_id, qr_hex in qr_info_tuples:
+            qr_info[qr_id] = qr_hex
 
         return qr_info
 
-    
-    def get_info(self, kit_id: int):
+    def get_info(self, identifier, log=False):
         """
-        -- logging --
-        Returns a dictionary containing kit information for the kit with the given kit_id.
-        If the kit does not exist, returns empty dict.
+        Returns a dictionary containing information about the kit with the given identifier, including its unique code,
+        creation and update times, status, owner details (if any), and a dictionary of QR codes associated with the kit.
+        If the kit does not exist, returns an empty dictionary.
         """
         kit_info_dict = {}
-        if not self.has(kit_id):
-            self.logger.log_message(f"Error: Kit #{kit_id} does not exist.")
-            return kit_info_dict
+        kit_id = self.has(identifier)
+        if not kit_id:
+            return self.logger.log(f"Error: Kit #{kit_id} does not exist.", kit_info_dict) if log else kit_info_dict
 
         with self.db as (conn, cursor):
-            cursor.execute("SELECT kit_unique_code, created_at, updated_at, kit_status, user_id FROM kits WHERE kit_id = %s", (kit_id,))
+            cursor.execute("""
+                SELECT unique_hex, created_at, updated_at, status, owner_id
+                FROM "kit"
+                WHERE id = %s
+            """, (kit_id,))
             kit_data = cursor.fetchone()
 
             if kit_data:
-                kit_info_dict['kit_unique_code'] = bytes(kit_data[0]).hex()
+                kit_info_dict['kit_unique_code'] = kit_data[0]
                 kit_info_dict['created_at'] = kit_data[1].strftime("%Y-%m-%d, %H:%M:%S")
                 kit_info_dict['updated_at'] = kit_data[2].strftime("%Y-%m-%d, %H:%M:%S")
-
-                cursor.execute("SELECT status_key FROM kit_statuses WHERE status_id = %s", (kit_data[3],))
-                kit_status_key = cursor.fetchone()[0]
-                kit_info_dict['kit_status'] = kit_status_key
+                kit_info_dict['kit_status'] = self.status_of(kit_id)
 
                 if kit_data[4]:  # Check if user_id is not None
-                    cursor.execute("SELECT user_id, user_name FROM users WHERE user_id = %s", (kit_data[4],))
+                    cursor.execute("""
+                        SELECT id, name
+                        FROM "user"
+                        WHERE id = %s
+                    """, (kit_data[4],))
                     owner_data = cursor.fetchone()
-                    owner_dict = {'user_id': owner_data[0], 'user_name': owner_data[1]} if owner_data else None
+                    owner_dict = {
+                        'user_id': owner_data[0], 'user_name': owner_data[1]} if owner_data else None
                     kit_info_dict['owner'] = owner_dict
                 else:
                     kit_info_dict['owner'] = None
-
                 kit_info_dict['qrs'] = self.get_qrs(kit_id)
-
         return kit_info_dict
 
     
     def get_all(self):
-        return self._all_getter("kit_id", "kits")
+        return self._all_getter("id", "kit")
 
-    
-    def new(self, n_qrs: int):
+    @multimethod
+    def new(self, n_qrs: int, log=False):
         """
-        -- logging --
         Inserts a new kit with `n_qrs` QRs into the database.
-        Returns the kit_id if successful, otherwise False.
+        Returns the kit_id.
         """
         if n_qrs > 50:
-            self.logger.log_message(f"Info : No more than 50 QRs in one kit!")
+            self.logger.log(f"Info : No more than 50 QRs in one kit!")
             n_qrs = 50
-        kit_unique_code = os.urandom(16)
-        qr_unique_codes = self._generate_qr_bytes(n_qrs, 10)
+        kit_unique_hex = os.urandom(8).hex()
+        qr_unique_hexes = self._generate_qr_bytes(n_qrs)
         
         with self.db as (conn, cursor):
             cursor.execute("""
-                INSERT INTO kits (kit_unique_code, n_qrs, kit_status)
-                VALUES (%s, %s, 1)
-                RETURNING kit_id
-            """, (kit_unique_code, n_qrs))
+                INSERT INTO "kit" (unique_hex, n_qrs)
+                VALUES (%s, %s)
+                RETURNING id
+            """, (kit_unique_hex, n_qrs))
             kit_id = cursor.fetchone()[0]
-            cursor.execute("UPDATE kit_statuses SET n = n + 1 WHERE status_id = 1")
-            for qr_unique_code in qr_unique_codes:
+            for qr_unique_code in qr_unique_hexes:
                 cursor.execute("""
-                    INSERT INTO qrs (qr_unique_code, kit_id)
+                    INSERT INTO "qr" (unique_hex, kit_id)
                     VALUES (%s, %s)
                 """, (qr_unique_code, kit_id))
             conn.commit()
-        self.logger.log_message(f"Info : Kit #{kit_id} with {n_qrs} QRs has been created")
-        return kit_id
+        return self.logger.log(f"Info : Kit #{kit_id} with {n_qrs} QRs has been created", kit_id) if log else kit_id
 
     
-    def change_status(self, kit_id, new_status):
-        return self._change_status("kit_id", "kits", "kit_status", "kit_statuses", kit_id, new_status)
+    def change_status(self, identifier, new_status, log=False):
+        return self._change_status("kit", identifier, new_status, log=log)
 
     
-    def change_owner(self, kit_id: int, user_name: str):
-        """
-        -- logging --
-        Changes the owner of a kit with the given kit_id to the user with the specified user_name.
-        Returns kit_id if successful, otherwise False.
-        """
-        if not self.has(kit_id):
-            self.logger.log_message(f"Error: Kit #{kit_id} does not exist.")
-            return False
+    def change_owner(self, identifier: int, new_owner: str, log=False):
+        kit_id = self.has(identifier, log=log)
+        if not kit_id:
+            return self.logger.log(f"Error: Kit #{kit_id} does not exist.", False) if log else False
 
         users = UsersManager(self.logdata, logfile=self.logfile)
-        user_id = users.has(user_name)
+        user_id = users.has(new_owner, log=log)
         if not user_id:
-            self.logger.log_message(f"Error: User '{user_name}' does not exist.")
-            return False
+            return self.logger.log(f"Error: User '{user_name}' does not exist.", False) if log else False
 
         with self.db as (conn, cursor):
-            cursor.execute("UPDATE kits SET user_id = %s WHERE kit_id = %s", (user_id, kit_id))
+            cursor.execute("""UPDATE "kit" SET owner_id = %s WHERE id = %s""", (user_id, kit_id))
             conn.commit()
 
-        self.logger.log_message(f"Info : Owner of Kit #{kit_id} changed to '{user_name}'")
-        return kit_id
+        return self.logger.log(f"Info : Owner of Kit #{kit_id} changed to '{user_name}'", kit_id) if log else kit_id
