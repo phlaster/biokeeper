532695fb48d7936005dcc5e81a4aeed3b323ae43
Name Name <you@example.com>
2024-05-19 18:14:19 +0300

    feat(UsersManager): Refactor `_validate_password()`, `_validate_user_name()`, `_hashing()`, `_hash_and_salt()`, `has()`, `status_of()`, `get_info()`, `new()`, `change_status()`, `rename()`, `change_user_password()`, and `password_match()` methods

--- python/src/DBM/UsersManager.py
+++ python/src/DBM/UsersManager.py
@@ -6,51 +6,39 @@ from multimethod import multimethod
 
 class UsersManager(AbstractDBManager):
     @multimethod
-    def _validate_password(self, password: str, user_name: str):
+    def _validate_password(self, password: str, user_name: str, log=False):
         """
-        --- logging ---
-
         Simplest validation possible. Rest is for the frontenders
         """
         if len(password) > 4:
             return True
         else:
-            self.logger.log_message(f"Error: Password validation for '{user_name}' failed.")
-            return False
+            return self.logger.log(f"Error: Password validation for '{user_name}' failed.", False) if log else False
     
     @multimethod
-    def _validate_user_name(self, user_name: str):
+    def _validate_user_name(self, user_name: str, log=False):
         """
-        --- logging ---
         Simplest validations for a user_name
         """
         if len(user_name) > 4:
             return True
         else:
-            self.logger.log_message(f"Error: Username '{user_name}' validation failed.")
-            return False
+            return self.logger.log(f"Error: Username '{user_name}' validation failed.", False) if log else False
     
     @multimethod
-    def _hashing(self, password_bytes: bytes, salt: bytes) -> bytes:
-        """
-        hashing password with salt
-        """
-        return hashlib.scrypt(password_bytes, salt=salt, n=2**14, r=8, p=1)
+    def _hashing(self, password: str, salt: str) -> str:
+        password_encoded = password.encode('utf-8')
+        salt_encoded = bytes.fromhex(salt)
+        return hashlib.scrypt(password_encoded, salt=salt_encoded, n=2**14, r=8, p=1).hex()
     
     @multimethod
-    def _hash_and_salt(self, password: str) -> tuple[bytes, bytes]:
-        """
-        Hashes a password with a salt using the scrypt algorithm.
-        """
-        salt = os.urandom(16)
-        password_bytes = password.encode('utf-8')
-        hashed = self._hashing(password_bytes, salt)
+    def _hash_and_salt(self, password: str) -> tuple[str, str]:
+        salt = os.urandom(16).hex()
+        hashed = self._hashing(password, salt)
         return (hashed, salt)
 
     def count(self, status: str = "all"):
         """
-        Returns number of users with different statuses.
-        'all' for all statuses OR
         Currently possible statuses: "admin", "volunteer", "observer"
         """
         return self._counter("user_statuses", status)
@@ -60,28 +48,28 @@ class UsersManager(AbstractDBManager):
         return self._is_status_of("user", status)
 
     @multimethod
-    def has(self, user_name: str):
-        return self._is("user_id", "users", "user_name", user_name)
+    def has(self, user_name: str, log=False):
+        id = self._SELECT("id", "user", "name", user_name)
+        if not id:
+            return self.logger.log(f"Error: No such user '{user_name}'.", 0) if log else 0
+        return id
     
     @multimethod
-    def has(self, user_id: int):
-        return self._is("user_name", "users", "user_id", user_id)
+    def has(self, user_id: int, log=False):
+        id = self._SELECT("id", "user", "id", user_id)
+        if not id:
+            return self.logger.log(f"Error: No such user #{user_id}.", 0) if log else 0
+        return id
 
     @multimethod
-    def status_of(self, user_name: str):
+    def status_of(self, identifier: str, log=False):
         """
-        -- logging --
         if user_name is incorrect returns False
         """
-        if not self.has(user_name):
-            self.logger.log_message(f"Error: Attempt to check status of an nonexisting user '{user_name}'.")
+        user_id = self.has(identifier, log=log)
+        if not user_id:
             return ""
-        return self._status_getter("user_status", "users", "user_name", "user_statuses", user_name)
-    
-    @multimethod
-    def status_of(self, user_id: int):
-        username = self.has(user_id)
-        return self.status_of(username)
+        return self._status_getter("user", user_id)
 
     @multimethod
     def get_info(self, user_name: str):
@@ -93,149 +81,126 @@ class UsersManager(AbstractDBManager):
 
         with self.db as (conn, cursor):
             cursor.execute("""
-                SELECT user_id, user_status, created_at, updated_at, n_samples_collected
-                FROM users
-                WHERE user_name = %s
+                SELECT id, status, created_at, updated_at, n_samples_collected
+                FROM "user"
+                WHERE name = %s
             """, (user_name,))
             user_data = cursor.fetchone()
 
-            if user_data:
-                user_info_dict['user_id'] = user_data[0]
+        if user_data:
+            user_info_dict['user_id'] = user_data[0]
 
-                # Fetching status_key from user_statuses table
-                cursor.execute("SELECT status_key FROM user_statuses WHERE status_id = %s", (user_data[1],))
-                status_key = cursor.fetchone()[0]
-                user_info_dict['user_status'] = status_key
+            status_key = self.status_of(user_name)
+            user_info_dict['user_status'] = status_key
 
-                user_info_dict['created_at'] = user_data[2].strftime("%Y-%m-%d, %H:%M:%S")
-                user_info_dict['updated_at'] = user_data[3].strftime("%Y-%m-%d, %H:%M:%S")
-                user_info_dict['n_samples_collected'] = user_data[4]
+            user_info_dict['created_at'] = user_data[2].strftime("%Y-%m-%d, %H:%M:%S")
+            user_info_dict['updated_at'] = user_data[3].strftime("%Y-%m-%d, %H:%M:%S")
+            user_info_dict['n_samples_collected'] = user_data[4]
         return user_info_dict
 
     @multimethod
-    def get_info(self, user_id: int):
-        username = self.has(user_id)
-        return self.status_of(username)
+    def get_info(self, user_id: int, log=False):
+        username = self.has(user_id, log=log)
+        return self.status_of(username, log=log)
 
     def get_all(self):
-        return self._all_getter("user_name", "users")
+        return self._all_getter("name", "user")
 
     @multimethod
-    def new(self, user_name: str, password: str):
+    def new(self, user_name: str, password: str, log=False):
         """
         -- logging --
         Returns the user_id if successful, otherwise False.
         """
-        if self.has(user_name):
-            self.logger.log_message(f"Error: Username '{user_name}' is already taken.")
+        if self.has(user_name, log=log):
             return False
-        if not self._validate_user_name(user_name):
+        if not self._validate_user_name(user_name, log=log):
             return False
-        if not self._validate_password(password, user_name):
+        if not self._validate_password(password, user_name, log=log):
             return False
         
         password_hash, salt = self._hash_and_salt(password)
 
         with self.db as (conn, cursor):
             cursor.execute("""
-                INSERT INTO users (user_name, user_status, password_hash, password_salt)
-                VALUES (%s, 3, %s, %s)
-                RETURNING user_id
+                INSERT INTO "user"
+                (name, password_hash, password_salt)
+                VALUES (%s, %s, %s)
+                RETURNING id
             """, (user_name, password_hash, salt))
             user_id = cursor.fetchone()[0]
-            cursor.execute("UPDATE user_statuses SET n = n + 1 WHERE status_id = 3")
             conn.commit()
-        self.logger.log_message(f"Info : User #{user_id} '{user_name}' has been created")
-        return user_id 
+        return self.logger.log(f"Info : User #{user_id} '{user_name}' has been created", user_id) if log else user_id
 
     @multimethod
-    def change_status(self, user_name: str, new_status: str):
-        return self._change_status("user_name", "users", "user_status", "user_statuses", user_name, new_status)
+    def change_status(self, identifier: str, new_status: str, log=False):
+        return self._change_status("user", identifier, new_status, log=log)
+
 
     @multimethod
-    def change_status(self, user_id, new_status):
-        username = self.has(user_id)
-        return self._change_status("user_name", "users", "user_status", "user_statuses", user_name, new_status)
-
-    @multimethod
-    def rename(self, old_user_name: str, new_user_name: str):
+    def rename(self, old_user_name: str, new_user_name: str, log=False):
         """
         -- logging --
         returns False if unsuccessfull
         """
-        if not self.has(old_user_name):
-            self.logger.log_message(f"Error: Can't change user_name of a nonexisting user '{old_user_name}'.")
-            return False
+        if not self.has(old_user_name, log=log):
+            return self.logger.log(f"Error: Can't change user_name of a nonexisting user '{old_user_name}'.", False) if log else False
         
         if old_user_name==new_user_name:
-            # No user_name change, no logging
             return old_user_name
 
-        if self.has(new_user_name):
-            self.logger.log_message(f"Error: Can't change user_name '{old_user_name}' to '{new_user_name}' - the name is taken.")
-            return False
+        if self.has(new_user_name, log=log):
+            return self.logger.log(f"Error: Can't change user_name '{old_user_name}' to '{new_user_name}' - the name is taken.", False) if log else False
 
-        if not self._validate_user_name(new_user_name):
+        if not self._validate_user_name(new_user_name, log=log):
             return False
         
         with self.db as (conn, cursor):
-            cursor.execute("UPDATE users SET user_name = %s WHERE user_name = %s", (new_user_name, old_user_name))
+            cursor.execute('UPDATE "user" SET name = %s WHERE name = %s', (new_user_name, old_user_name))
             conn.commit()
             
-        self.logger.log_message(f"Info : User '{old_user_name}' changed name to '{new_user_name}'.")
-        return new_user_name
+        return self.logger.log(f"Info : User '{old_user_name}' changed name to '{new_user_name}'.", new_user_name) if log else new_user_name
 
     @multimethod
-    def rename(self, user_id: int, new_user_name: str):
+    def rename(self, user_id: int, new_user_name: str, log=False):
         old_user_name = self.has(user_id)
-        return self.rename(self, old_user_name, new_user_name)
+        return self.rename(self, old_user_name, new_user_name, log=log)
     
     @multimethod
-    def change_user_password(self, user_name: str, new_password: str):
+    def change_user_password(self, user_name: str, new_password: str, log=False):
         """
         -- logging --
         returns False if unsuccessfull
         """
-        if not self.has(user_name):
-            self.logger.log_message(f"Error: Can't change password of a nonexisting user '{user_name}'.")
-            return False
+        if not self.has(user_name, log=log):
+            return self.logger.log(f"Error: Can't change password of a nonexisting user '{user_name}'.", False) if log else False
         
-        if not self._validate_password(new_password, user_name):
+        if not self._validate_password(new_password, user_name, log=log):
             return False
         
         with self.db as (conn, cursor):
             hashed_password, salt = self._hash_and_salt(new_password)
             cursor.execute("""
-                UPDATE users
+                UPDATE "user"
                 SET password_hash = %s, password_salt = %s
                 WHERE user_name = %s;
             """, (hashed_password, salt, user_name))
             conn.commit()
-        self.logger.log_message(f"Info : Changed password for user '{user_name}'.")
-        return True
+        return self.logger.log(f"Info : Changed password for user '{user_name}'.", True) if log else True
 
     @multimethod
-    def change_user_password(self, user_id: str, new_password: str):
+    def change_user_password(self, user_id: str, new_password: str, log=False):
         user_name = self.has(user_id)
-        return self.change_user_password(user_name, new_password)
+        return self.change_user_password(user_name, new_password, log=log)
     
-    @multimethod
-    def password_match(self, user_name: str, password: str):
-        """
-        -- logging --
-        """
-        if not self.has(user_name):
-            self.logger.log_message(f"Error: Authentication attempt for nonexisting user_name '{user_name}'.")
-            return False
+    def password_match(self, identifier, password: str, log=False):
+        user_id = self.has(identifier, log=log)
+        if not user_id:
+            return self.logger.log(f"Error: Authentication attempt for nonexisting user '{identifier}'.", False) if log else False
 
         with self.db as (conn, cursor):
-            cursor.execute("SELECT password_hash, password_salt FROM users WHERE user_name = %s", (user_name,))
+            cursor.execute('SELECT password_hash, password_salt FROM "user" WHERE id = %s', (user_id,))
             stored_hash, stored_salt = cursor.fetchone()
-        rehashed_password = self._hashing(password.encode('utf-8'), bytes(stored_salt))
+        rehashed_password = self._hashing(password, stored_salt)
 
-        return rehashed_password == bytes(stored_hash)
-
-    @multimethod
-    def password_match(self, user_id: int, password: str):
-        user_name = self.has(user_id)
-        return self.password_match(user_name, password)
+        return rehashed_password == stored_hash
