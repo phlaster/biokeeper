c2cf6643ba0f8587d7eea6451c21d31ae2f5065b
Name Name <you@example.com>
2024-05-19 18:13:59 +0300

    feat(DBManager): Refactor `_counter()`, `_is_status_of()`, `_SELECT()`, `_status_getter()`, and `_change_status()` methods

--- python/src/DBM/ADBM.py
+++ python/src/DBM/ADBM.py
@@ -10,92 +10,87 @@ class AbstractDBManager(ABC):
         self.logfile = logfile
         self.logger = Logger(logfile)
     
-    def _counter(self, table_name:str, status_key: str = "all"):
+    def _counter(self, table_name: str, status_key: str = "all"):
         with self.db as (conn, cursor):
-            if status_key == 'all':
-                cursor.execute(f"SELECT SUM(n) FROM {table_name};")
-            else:
-                cursor.execute(f"SELECT n FROM {table_name} WHERE status_key = %s;", (status_key,))
-            result = cursor.fetchone()[0]
-        return result
+            query = f"""
+                SELECT SUM((details).n)
+                FROM "{table_name}"
+                {"" if status_key == "all" else "WHERE (details).key = %s"}
+            """
+            cursor.execute(query, (status_key,) if status_key != "all" else ())
+            return cursor.fetchone()[0]
 
-    def _is_status_of(self, table_prefix:str, status:str):
-        """
-        -- logging --
-        returns (status_id, status_info) if successfull;
-        Examples:
-        is_status_of("user", "admin") == True
-        is_status_of("kit", "thrown away") == False     # kit has no status 'thrown away'
-        """
-        with DBConnection(self.logdata) as (conn, cursor):
-            table_name = f"{table_prefix}_statuses"
-            try:
-                cursor.execute(f"""
-                    SELECT status_id, status_info
-                    FROM {table_name}
-                    WHERE status_key = '{status}'
-                """)
-                result = cursor.fetchone()
-            except Exception as e:
-                self.logger.log_message(f"Error: {e}".split("\n")[0])
-                return ()
-        
-        return result if result else ()
 
-    def _is(self, identifier_id:str, table:str, column:str, identifier:str):
+    def _is_status_of(self, table_prefix: str, status: str) -> tuple[int, str] | tuple:
         """
-        returns research_id if successfull
+        Check if a given status exists for a table with given `table_prefix`.
         """
-        with DBConnection(self.logdata) as (conn, cursor):
-            cursor.execute(f"SELECT {identifier_id} FROM {table} WHERE {column} = %s", (identifier,))
+        table_name = f"{table_prefix}_statuses"
+        query = f"""
+            SELECT id, (details).info
+            FROM {table_name}
+            WHERE (details).key = %s;"""
+
+        with self.db as (conn, cursor):
+            cursor.execute(query, (status,))
+            return cursor.fetchone() or ()
+
+    def _SELECT(self, what:str, table:str, where:str, val):
+        with self.db as (conn, cursor):
+            cursor.execute(f"""
+                SELECT {what}
+                FROM "{table}"
+                WHERE {where} = %s
+            """, (val,))
             result = cursor.fetchone()
-        return result[0] if result else 0
+        return result[0] if result else None
 
-    def _status_getter(self, status_column:str, obj_table:str, identifier_name:str, status_table:str, identifier:str):
-        """
-        -- logging --
-        if user_name is incorrect returns False
-        """
+    def _status_getter(self, table:str, id: int):
         with self.db as (conn, cursor):
-            cursor.execute(f"SELECT {status_column} FROM {obj_table} WHERE {identifier_name} = %s", (identifier,))
-            status_id = cursor.fetchone()
-            cursor.execute(f"SELECT status_key FROM {status_table} WHERE status_id = %s", (status_id,))
-            status_key = cursor.fetchone()[0]
-        return status_key
+            cursor.execute(f"""
+                SELECT (details).key, (details).info
+                FROM {table}_statuses
+                WHERE id = (
+                    SELECT status
+                    FROM "{table}"
+                    WHERE id = {id}
+                );
+            """)
+            status_key = cursor.fetchone()
+        return status_key[0] if status_key else None
 
-    def _change_status(self, identifier_name, identifier_table, status_column, statuses_table, identifier, new_status):
-        """
-        -- logging --
-        returns False if unsuccessfull
-        """
-        if not self.has(identifier):
-            if identifier == "kit":
-                self.logger.log_message(f"Error: No such {identifier_name.split('_')[0]} '{identifier}'.")
-            else:
-                self.logger.log_message(f"Error: Kit #{identifier} does not exist.")
-            return ""
+    def _change_status(self, table, identifier, new_status, log=False):
+        id = self.has(identifier)
+        if not id:
+            return self.logger.log(f"Error: Couldn't change status of {table} {identifier}: Does not exist.", "") if log else ""
 
-        new_status_query = self.has_status(new_status)
-        if not new_status_query:
-            self.logger.log_message(f"Error: Status '{new_status}' is not a valid {identifier_name.split('_')[0]} status.")
-            return ""
-        new_status_id = new_status_query[0]
+        new_status_id = self.has_status(new_status)
+        if not new_status_id:
+            return self.logger.log(f"Error: Couldn't change status of {table} {identifier}: Status '{new_status}' does not exist.", "") if log else ""
 
-        current_status = self.status_of(identifier)
-        current_status_id = self.has_status(current_status)[0]
+        current_status_id = self.has_status(self.status_of(identifier))[0]
 
         if new_status_id == current_status_id:
-            # No status change, no logging
             return new_status
 
         with self.db as (conn, cursor):
-            cursor.execute(f"UPDATE {identifier_table} SET {status_column} = %s WHERE {identifier_name} = %s", (new_status_id, identifier))
-            cursor.execute(f"UPDATE {statuses_table} SET n = n - 1 WHERE status_id = %s", (current_status_id,))
-            cursor.execute(f"UPDATE {statuses_table} SET n = n + 1 WHERE status_id = %s", (new_status_id,))
+            cursor.execute(
+                f"""
+                UPDATE "{table}"
+                SET status = (
+                    SELECT id
+                    FROM "{table}_statuses"
+                    WHERE (details).key = %s
+                )
+                WHERE id = %s
+                """,
+                (new_status, id),
+            )
             conn.commit()
 
-        self.logger.log_message(f"Info : Research '{identifier}' status changed to '{new_status}'")
+        log and self.logger.log(f"Info : {table} '{identifier}' status changed to '{new_status}'")
         return new_status
+
 
     def _all_getter(self, identifier_name, table_name):
         """
@@ -105,38 +100,41 @@ class AbstractDBManager(ABC):
         alls = {}
 
         with self.db as (conn, cursor):
-            cursor.execute(f"SELECT {identifier_name} FROM {table_name}")
+            cursor.execute(f"""
+            SELECT {identifier_name}
+            FROM "{table_name}"
+            """)
             all_identifiers = cursor.fetchall()
 
-        for tpl in all_identifiers:
-            obj_id = tpl[0]
-            kit_info = self.get_info(obj_id)
-            alls[obj_id] = kit_info
+        for row in all_identifiers:
+            obj_id = row[0]
+            obj_info = self.get_info(obj_id)
+            alls[obj_id] = obj_info
 
         return alls
     
     @abstractmethod
-    def count(self, status):
+    def count(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def has_status(self, status):
+    def has_status(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def has(self, identifier):
+    def has(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def status_of(self, identifier):
+    def status_of(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def get_info(self, identifier):
+    def get_info(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def get_all(self):
+    def get_all(self, *args, **kwargs):
         pass
 
     @abstractmethod
@@ -144,26 +142,26 @@ class AbstractDBManager(ABC):
         pass
 
     @abstractmethod
-    def change_status(self, identifier, new_status):
+    def change_status(self, *args, **kwargs):
         pass
-
 
     def clear_logs(self):
         self.logger.clear_logs()
 
     @multimethod
-    def get_qr_info(self, qr_bytes: bytes):
+    def get_qr_info(self, qr_hex: str) -> dict:
         """
-        returns dict with QR info if successfull
-        empty dict otherwise
+        Retrieves information about a QR code from the database.
         """
         with self.db as (conn, cursor):
-            cursor.execute("SELECT qr_id, is_used, kit_id FROM qrs WHERE qr_unique_code = %s", (qr_bytes,))
+            cursor.execute("""
+                SELECT id, is_used, kit_id
+                FROM "qr"
+                WHERE unique_hex = %s
+            """, (qr_hex,))
             result = cursor.fetchone()
-
-        return {'qr_id': int(result[0]), 'is_used': bool(result[1]), 'kit_id': int(result[2])} if result else {}
-
-    @multimethod
-    def get_qr_info(self, qr_hex: str):
-        qr_bytes = bytes.fromhex(qr_hex)
-        return self.get_qr_info(qr_bytes)
+        return {
+            'qr_id': int(result[0]),
+            'is_used': bool(result[1]),
+            'kit_id': int(result[2])
+        } if result else {}
