3bda706608d954cb9f189291ae477e28f115fe09
Name Name <you@example.com>
2024-05-19 18:14:44 +0300

    feat(ResearchesManager): Refactor `has()`, `status_of()`, `get_info()`, `new()`, `change_status()`, `change_comment()`, and `change_day_end()` methods

--- python/src/DBM/ResearchesManager.py
+++ python/src/DBM/ResearchesManager.py
@@ -1,148 +1,143 @@
 from DBM.ADBM import AbstractDBManager
 from DBM.UsersManager import UsersManager
 import datetime
+from multimethod import multimethod
 
 class ResearchesManager(AbstractDBManager):
     def count(self, status:str="all"):
         """
-        Returns number of researches with different statuses.
-        'all' for all statuses OR
         Currently possible statuses: "pending", "ongoing", "paused", "ended", "cancelled"
         """
         return self._counter("research_statuses", status)
 
-    
-    def has_status(self, status):
+    @multimethod
+    def has_status(self, status: str):
         return self._is_status_of("research", status)
 
+    @multimethod
+    def has(self, research_name: str, log=False):
+        id = self._SELECT("id", "research", "name", research_name)
+        if not id:
+            return self.logger.log(f"Error: No such research '{research_name}'.", 0) if log else 0
+        return id
     
-    def has(self, research_name):
-        return self._is("research_id", "researches", "research_name", research_name)
+    @multimethod
+    def has(self, research_id: int, log=False):
+        id = self._SELECT("id", "research", "id", research_id)
+        if not id:
+            return self.logger.log(f"Error: No such research #{research_id}.", 0) if log else 0
+        return id
 
     
-    def status_of(self, research_name):
+    def status_of(self, identifier, log=False):
         """
         Returns the status key of the research with the given name.
         If the research does not exist, False is returned.
         """
-        if not self.has(research_name):
-            self.logger.log_message(f"Error: Research '{research_name}' does not exist.")
-            return ""
-        return self._status_getter("research_status", "researches", "research_name", "research_statuses", research_name)
+        id = self.has(identifier)
+        if not id:
+            return self.logger.log(f"Error: Research '{identifier}' does not exist.", "") if log else ""
+        return self._status_getter("research", id)
 
     
-    def get_info(self, research_name: str):
+    def get_info(self, research_name: str, log=False):
         """
         Returns research information as a dictionary for the given research name.
         If the research does not exist, an empty dictionary is returned (equivalent to False).
         """
         research_info_dict = {}
         if not self.has(research_name):
-            self.logger.log_message(f"Error: Research '{research_name}' does not exist.")
-            return research_info_dict
+            return self.logger.log(f"Error: Research '{research_name}' does not exist.", research_info_dict) if log else research_info_dict
 
         with self.db as (conn, cursor):
             cursor.execute("""
-                SELECT research_id, research_status, created_at, updated_at, created_by, day_start, day_end, n_samples, research_comment
-                FROM researches
-                WHERE research_name = %s
+                SELECT id, status, created_at, updated_at, created_by, day_start, day_end, n_samples, comment
+                FROM "research"
+                WHERE name = %s
             """, (research_name,))
             research_data = cursor.fetchone()
 
-            if research_data:
-                research_info_dict['research_id'] = research_data[0]
-                cursor.execute("SELECT status_key FROM research_statuses WHERE status_id = %s", (research_data[1],))
-                status_key = cursor.fetchone()[0]
-                research_info_dict['research_status'] = status_key
-                research_info_dict['created_at'] = research_data[2].strftime("%Y-%m-%d, %H:%M:%S")
-                research_info_dict['updated_at'] = research_data[3].strftime("%Y-%m-%d, %H:%M:%S")
-                research_info_dict['created_by'] = research_data[4]
-                research_info_dict['day_start'] = research_data[5].strftime("%Y-%m-%d")
-                research_info_dict['day_end'] = research_data[6].strftime("%Y-%m-%d") if research_data[6] else None
-                research_info_dict['n_samples'] = research_data[7]
-                research_info_dict['research_comment'] = research_data[8]
+        if research_data:
+            research_info_dict['research_id'] = research_data[0]
+            research_info_dict['research_status'] = self.status_of(research_name)
+            research_info_dict['created_at'] = research_data[2].strftime("%Y-%m-%d, %H:%M:%S")
+            research_info_dict['updated_at'] = research_data[3].strftime("%Y-%m-%d, %H:%M:%S")
+            research_info_dict['created_by'] = research_data[4]
+            research_info_dict['day_start'] = research_data[5].strftime("%Y-%m-%d")
+            research_info_dict['day_end'] = research_data[6].strftime("%Y-%m-%d") if research_data[6] else None
+            research_info_dict['n_samples'] = research_data[7]
+            research_info_dict['research_comment'] = research_data[8]
 
         return research_info_dict
 
 
     def get_all(self):
-        return self._all_getter("research_name", "researches")
+        return self._all_getter("name", "research")
 
     
-    def new(self, research_name: str, user_name: str, day_start: datetime.date, research_comment: str = None):
+    def new(self, research_name: str, user_name: str, day_start: datetime.date, research_comment: str = None, log=False):
         """
         -- logging --
         Returns the research_id if successful, otherwise False.
         """
         if self.has(research_name):
-            self.logger.log_message(f"Error: Research '{research_name}' is already exists.")
-            return False
+            return self.logger.log(f"Error: Research '{research_name}' is already exists.", False) if log else False
 
         users = UsersManager(self.logdata, logfile=self.logfile)
         user_id = users.has(user_name)
         if not user_id:
-            self.logger.log_message(f"Error: Can't assign new research '{research_name}' to a nonexisting user '{user_name}'.")
-            return False
+            return self.logger.log(f"Error: Can't assign new research '{research_name}' to a nonexisting user '{user_name}'.", False) if log else False
 
         user_status = users.status_of(user_name) # Careful here!
         if user_status != "admin":
-            self.logger.log_message(f"Error: User '{user_name}' of status '{user_status}' has no privilege to create researches.")
-            return False
+            return self.logger.log(f"Error: User '{user_name}' of status '{user_status}' has no privilege to create researches.", False) if log else False
 
         with self.db as (conn, cursor):
-            # Insert new research
             cursor.execute("""
-                INSERT INTO researches (research_name, research_status, research_comment, created_by, day_start)
-                VALUES (%s, 1, %s, %s, %s)
-                RETURNING research_id
+                INSERT INTO "research"
+                (name, comment, created_by, day_start)
+                VALUES (%s, %s, %s, %s)
+                RETURNING id
             """, (research_name, research_comment, user_id, day_start))
             research_id = cursor.fetchone()[0]
-            cursor.execute("UPDATE research_statuses SET n = n + 1 WHERE status_id = 1")
             conn.commit()
-        self.logger.log_message(f"Info : Research #{research_id} '{research_name}' starting on {day_start} created by '{user_name}'")
-        return research_id
+        return self.logger.log(f"Info : Research #{research_id} '{research_name}' starting on {day_start} created by '{user_name}'", research_id) if log else research_id
 
     
-    def change_status(self, research_name, new_status):
-        return self._change_status("research_name", "researches", "research_status", "research_statuses", research_name, new_status)
+    def change_status(self, identifier, new_status, log=False):
+        return self._change_status("research", identifier, new_status, log)
 
     
-    def change_comment(self, research_name: str, comment: str):
-        """
-        -- logging --
-        returns False if unsuccessfull
-        """
+    def change_comment(self, research_name: str, comment: str, log=False):
         if not self.has(research_name):
-            self.logger.log_message(f"Error: Research '{research_name}' does not exist.")
-            return False
+            return self.logger.log(f"Error: Research '{research_name}' does not exist.", False) if log else False
 
         with self.db as (conn, cursor):
-            cursor.execute("UPDATE researches SET research_comment = %s WHERE research_name = %s", (comment, research_name))
+            cursor.execute("""
+                UPDATE "research"
+                SET comment = %s
+                WHERE name = %s
+            """, (comment, research_name))
             conn.commit()
 
-        self.logger.log_message(f"Info : Updated comment for research '{research_name}'")
-        return True
+        return self.logger.log(f"Info : Updated comment for research '{research_name}'", True) if log else True
 
     
-    def change_day_end(self, research_name: str, day_end: datetime.date):
-        """
-        -- logging --
-        returns False if unsuccessfull
-        """
+    def change_day_end(self, research_name: str, day_end: datetime.date, log=False):
         if not self.has(research_name):
-            self.logger.log_message(f"Error: Research '{research_name}' does not exist.")
-            return False
+            return self.logger.log(f"Error: Research '{research_name}' does not exist.", False) if log else False
 
         with self.db as (conn, cursor):
-            cursor.execute("SELECT day_start FROM researches WHERE research_name = %s", (research_name,))
-            day_start = cursor.fetchone()[0]
+            day_start = self._SELECT("day_start", "research", "name", research_name)
 
             if day_end < day_start:
-                self.logger.log_message(f"Error: Can't set ending day at {day_end} for research '{research_name}' that starts on {day_start}.")
-                return False
+                return self.logger.log(f"Error: Can't set ending day at {day_end} for research '{research_name}' that starts on {day_start}.", False) if log else False
 
-            cursor.execute("UPDATE researches SET day_end = %s WHERE research_name = %s", (day_end, research_name))
+            cursor.execute("""
+                UPDATE "research"
+                SET day_end = %s
+                WHERE name = %s
+            """, (day_end, research_name))
             conn.commit()
 
-        self.logger.log_message(f"Info : Now '{research_name}' ends on {day_end}")
-        return True
+        return self.logger.log(f"Info : Now '{research_name}' ends on {day_end}", True) if log else True
