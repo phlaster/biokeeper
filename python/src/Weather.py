e348d8b4cdb5a109ef0decb66e5b9010e1036c93
Name Name <you@example.com>
2024-05-20 19:42:27 +0300

    wrapped weather_api call into try block in case of connection errors (offline testing)

--- python/src/Weather.py
+++ python/src/Weather.py
@@ -40,25 +40,27 @@ class Weather:
             "start_date": (timestamp-pd.Timedelta(days=self.past_days)).strftime("%Y-%m-%d"),
             "end_date": timestamp.strftime("%Y-%m-%d")
         }
-        responses = self.openmeteo.weather_api(self.api_url, params=params)
-
-        response = responses[0]
-        hourly = response.Hourly()
-        hourly_data = {
-            "date": pd.date_range(
-                start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-                end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
-                freq=pd.Timedelta(seconds=hourly.Interval()),
-                inclusive="left"
-            ),
-            "temperature_2m": hourly.Variables(0).ValuesAsNumpy(),
-            "relative_humidity_2m": hourly.Variables(1).ValuesAsNumpy(),
-            "soil_temperature_0cm": hourly.Variables(2).ValuesAsNumpy(),
-            "soil_temperature_6cm": hourly.Variables(3).ValuesAsNumpy(),
-            "soil_moisture_0_to_1cm": hourly.Variables(4).ValuesAsNumpy(),
-            "soil_moisture_1_to_3cm": hourly.Variables(5).ValuesAsNumpy(),
-            "soil_moisture_3_to_9cm": hourly.Variables(6).ValuesAsNumpy(),
-            "uv_index": hourly.Variables(7).ValuesAsNumpy()
-        }
-        hourly_dataframe = pd.DataFrame(data=hourly_data)
-        return hourly_dataframe.to_json()
+        try:
+            responses = self.openmeteo.weather_api(self.api_url, params=params)
+            response = responses[0]
+            hourly = response.Hourly()
+            hourly_data = {
+                "date": pd.date_range(
+                    start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+                    end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+                    freq=pd.Timedelta(seconds=hourly.Interval()),
+                    inclusive="left"
+                ),
+                "temperature_2m": hourly.Variables(0).ValuesAsNumpy(),
+                "relative_humidity_2m": hourly.Variables(1).ValuesAsNumpy(),
+                "soil_temperature_0cm": hourly.Variables(2).ValuesAsNumpy(),
+                "soil_temperature_6cm": hourly.Variables(3).ValuesAsNumpy(),
+                "soil_moisture_0_to_1cm": hourly.Variables(4).ValuesAsNumpy(),
+                "soil_moisture_1_to_3cm": hourly.Variables(5).ValuesAsNumpy(),
+                "soil_moisture_3_to_9cm": hourly.Variables(6).ValuesAsNumpy(),
+                "uv_index": hourly.Variables(7).ValuesAsNumpy()
+            }
+            hourly_dataframe = pd.DataFrame(data=hourly_data)
+            return hourly_dataframe.to_json()
+        except ConnectionError as e: # for offline testing
+            return None
