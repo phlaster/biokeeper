db14ec57e43f91f6cbbb0b6874cd2af66649ce3c
Name Name <you@example.com>
2024-05-21 19:24:12 +0300

    new test file

---
+++ python/test/interactive_tests.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('python/src')
+from DBManager import DBManager, LOGDATA
+
+DBM = DBManager(LOGDATA, '/dev/null')
+
+w = DBM.samples.get_weather(3)
+# print(w)
+
+p = DBM.samples.get_photo(3)
+# print(bytes(p))
+
+c = DBM.samples.get_info(3)["user_comment"]
+print(c)
