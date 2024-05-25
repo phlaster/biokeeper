6cf6d180cc7abcc79a974e7e196c3de43486111b
Phlaster <you@example.com>
2024-05-25 13:39:33 +0300

    match_password response data fix

--- python/src/FastAPI/users.py
+++ python/src/FastAPI/users.py
@@ -43,9 +43,14 @@ def create_user(
 def get_user_score(user_name):
     return DBM.users.get_info(user_name)['n_samples_collected']
 
-@router.get('/users/password_match/{user_id}/{password}')
-def check_password_match(user_id: int, password: str):
+@router.get('/users/password_match/{user_name}/{password}')
+def check_password_match(user_name: str, password: str):
+    user_data = DBM.users.get_info(user_name)
+    user_id = user_data['id']
+    print(user_data)
+    if not user_data:
+        return JSONResponse(status_code=401, content={'success': False})
     if user_id == DBM.users.password_match(user_id, password):
-        return JSONResponse(status_code=200, content={'success': True, 'user_id': user_id, 'user_status': 'admin'})
+        return JSONResponse(status_code=200, content={'success': True, 'user_id': user_id, 'user_name': user_data['name'], 'user_status': user_data['status']})
     return JSONResponse(status_code=401, content={'success': False})
         
