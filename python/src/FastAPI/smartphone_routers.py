ff78abaf3be805004f13f26713abfbfed700870d
Phlaster <you@example.com>
2024-05-25 15:19:29 +0300

    added JSONResponses

--- python/src/FastAPI/smartphone_routers.py
+++ python/src/FastAPI/smartphone_routers.py
@@ -1,6 +1,7 @@
 from fastapi.routing import APIRouter
 from db_manager import DBM
 from datetime import datetime
+from fastapi.responses import JSONResponse
 
 
 router = APIRouter()
@@ -9,86 +10,116 @@ router = APIRouter()
 async def react_login(username: str, password: str):
     user_id = DBM.users.password_match(username, password)
     if not user_id:
-        return {
-        "result" : False,
-        "response": "Failed authentication"
-    }
+        return JSONResponse(
+            content={
+                "result": False,
+                "response": "Failed authentication"
+            },
+            status_code=401
+        )
     
-    return {
-        "result" : True,
-        "response": f"Successfull log in for user {user_id}"
-    }
+    return JSONResponse(
+        content={
+            "result": True,
+            "response": f"Successful log in for user {user_id}"
+        },
+        status_code=200
+    )
 
 
 @router.post("/react/researches")
 async def react_ongoing_researches(username: str, password: str):
     user_id = DBM.users.password_match(username, password)
     if not user_id:
-        return {
-        "result" : False,
-        "response": "Failed authentication"
-    }
+        return JSONResponse(
+            content={
+                "result": False,
+                "response": "Failed authentication"
+            },
+            status_code=401
+        )
 
     researches = DBM.researches.get_all()
     if not researches:
-        return {
-        "result" : False,
-        "response": "No researches found"
-    }
+        return JSONResponse(
+            content={
+                "result": False,
+                "response": "No researches found"
+            },
+            status_code=404
+        )
     
     filtered_researches = {k: v for k, v in researches.items() if v['status'] == 'ongoing'}
     if not filtered_researches:
-        return {
-        "result" : False,
-        "response": "No ongoing researches found"
-    }
+        return JSONResponse(
+            content={
+                "result": False,
+                "response": "No ongoing researches found"
+            },
+            status_code=404
+        )
 
-    return {
-        "result" : True,
-        "response": filtered_researches
-    }
+    return JSONResponse(
+        content={
+            "result": True,
+            "response": filtered_researches
+        },
+        status_code=200
+    )
 
 
 @router.post("/react/user_stats")
 async def react_user_stats(username: str, password: str):
     user_id = DBM.users.password_match(username, password)
     if not user_id:
-        return {
-        "result" : False,
-        "response": "Failed authentication"
-    }
+        return JSONResponse(
+            content={
+                "result": False,
+                "response": "Failed authentication"
+            },
+            status_code=401
+        )
 
-    return {
-        "result" : True,
-        "response": DBM.users.get_info(username)
-    }
+    return JSONResponse(
+        content={
+            "result": True,
+            "response": DBM.users.get_info(username)
+        },
+        status_code=200
+    )
 
 
 @router.post("/react/push_sample")
 async def react_push_sample(
     username: str,
     password: str,
-    qr_unique_hex:str,
-    research_name:str,
-    collected_at:str,
-    latitude:float,
-    longitude:float
+    qr_unique_hex: str,
+    research_name: str,
+    collected_at: str,
+    latitude: float,
+    longitude: float
 ):
     user_id = DBM.users.password_match(username, password)
     if not user_id:
-        return {
-        "result" : False,
-        "response": "Failed authentication"
-    }
+        return JSONResponse(
+            content={
+                "result": False,
+                "response": "Failed authentication"
+            },
+            status_code=401
+        )
     
     # '2023-10-12T14:30:00+02:00'
     try:
         collected_at = datetime.fromisoformat(collected_at)
     except ValueError:
-        return {
-        "result" : False,
-        "response": "Incorrect datetime format. Use ISO: '2000-12-31T12:34:56+07:00'"
-    }
+        return JSONResponse(
+            content={
+                "result": False,
+                "response": "Incorrect datetime format. Use ISO: '2000-12-31T12:34:56+07:00'"
+            },
+            status_code=400
+        )
 
     response = DBM.samples.new(
         qr_unique_hex,
@@ -97,7 +128,19 @@ async def react_push_sample(
         (latitude, longitude),
         log=True
     )
-    return {
-        "result" : bool(response),
-        "response": response
-    }
+    if response:
+        return JSONResponse(
+            content={
+                "result": True,
+                "response": response
+            },
+            status_code=201
+        )
+    else:
+        return JSONResponse(
+            content={
+                "result": False,
+                "response": "Failed to push sample"
+            },
+            status_code=500
+        )
