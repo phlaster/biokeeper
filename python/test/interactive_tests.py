2af380f3a872c8e2a5c50bc74be7de21bf6d2fb8
Phlaster <you@example.com>
2024-05-25 13:21:01 +0300

    ignore interactive tests

--- python/test/interactive_tests.py
+++ python/test/interactive_tests.py
@@ -2,13 +2,25 @@ import sys
 sys.path.append('python/src')
 from DBManager import DBManager, LOGDATA
 
+import datetime
+import time
+
 DBM = DBManager(LOGDATA, '/dev/null')
 
-w = DBM.samples.get_weather(3)
-# print(w)
+# DBM.generate_test_example()
+# DBM.generate_test_example()
+# DBM.generate_test_example()
+# DBM.generate_test_example()
+# DBM.generate_test_example()
 
-p = DBM.samples.get_photo(3)
-# print(bytes(p))
+# DBM.users.new("Artiom", "password")
+print(DBM.users.status_of("Artiom"))
 
-c = DBM.samples.get_info(3)["comment"]
-print(c)
+# w = DBM.samples.get_weather(3)
+# # print(w)
+
+# p = DBM.samples.get_photo(3)
+# # print(bytes(p))
+
+# c = DBM.samples.get_info(3)["comment"]
+# print(c)
